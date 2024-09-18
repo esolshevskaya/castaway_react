e50991b6a3395ba5ebae4167e38253e2899ad83f
Alena Olshevskaya <71763361+esolshevskaya@users.noreply.github.com>
2024-09-18 21:06:03 +0300

    Add files via upload

---
+++ src/sections/Episodes/data.ts
@@ -0,0 +1,34 @@
+import episode3Img from '../../assets/images/episodes/episode3.jpg'
+import episode2Img from '../../assets/images/episodes/episode2.jpg'
+import episode1Img from '../../assets/images/episodes/episode2.jpg'
+
+
+export const episodesList = [
+    {
+        link: '#',
+        preview: episode3Img,
+        tag: 'Gear',
+        subtitle: 'Episode 3',
+        title: 'Should you get outboard audio gear?',
+        description: `Is hardware really worth it when it comes to podcasting? The answer is...it depends.
+        Here’s our reasons on why you might want to consider picking something up.`,
+    },
+    {
+        link: '#',
+        preview: episode2Img,
+        tag: 'Tips & Tricks',
+        subtitle: 'Episode 2',
+        title: 'Mic tricks to take you to the next level',
+        description: `Stop rolling with those default settings on your mic. These small tweaks will take you from sounding good to great.
+        `,
+    },
+    {
+        link: '#',
+        preview: episode1Img,
+        tag: 'Gear',
+        subtitle: 'Episode 1',
+        title: 'The best microphone under $200',
+        description: `With so many microphones on the market, how are you supposed to know what’s the best? Take a look at our top picks.                  </p>
+        `,
+    }
+]
