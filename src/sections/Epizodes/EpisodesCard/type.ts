c88a8c70f619dab2f72de78d9c8950e1c0b3f15f
Alena Olshevskaya <71763361+esolshevskaya@users.noreply.github.com>
2024-09-18 21:07:26 +0300

    Delete src/sections/Epizodes directory

--- src/sections/Epizodes/EpisodesCard/type.ts
+++
@@ -1,9 +0,0 @@
-export type TEpisodesCardProps = {
-    link: string,
-    preview: string,
-    tag: string,
-    subtitle: string,
-    title: string,
-    description: string,
-    linkText?: string
-}
